5a840fedcdb2d9c4fefbacac83bbad7a83b17046
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2012-10-30 12:44:20 -0400

    Add sample SQL files for webinar.

---
+++ sample_akiban_schema.sql
@@ -0,0 +1,49 @@
+drop table if exists relationship;
+drop table if exists post;
+drop table if exists photo;
+drop table if exists role;
+drop table if exists user;
+
+create table user
+(
+  id int not null primary key,
+  name varchar(128) not null,
+  email varchar(256)
+) engine=akibandb;
+
+create table post
+(
+  id int not null primary key,
+  user_id int not null,
+  status varchar(256) not null,
+  posted_at datetime not null,
+  latitude decimal(11, 7),
+  longitude decimal(11,7),
+  constraint`__akiban_fk_00` foreign key (user_id) references user (id)
+) engine=akibandb;
+
+create table role
+(
+  user_id int not null,
+  role_name varchar(64) not null,
+  primary key(user_id, role_name),
+  constraint`__akiban_fk_01` foreign key (user_id) references user (id)
+) engine=akibandb;
+
+create table photo
+(
+  id int not null primary key,
+  user_id int not null,
+  state varchar(64) not null,
+  date_added datetime,
+  path varchar(256) not null,
+  constraint`__akiban_fk_02` foreign key (user_id) references user (id)
+) engine=akibandb;
+
+create table relationship
+(
+  follower_id int not null,
+  followed_id int not null,
+  primary key(follower_id, followed_id),
+  constraint`__akiban_fk_03` foreign key (follower_id) references user (id)
+) engine=akibandb;
